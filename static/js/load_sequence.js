c42544e26010e74ce871ddd56a29036610aa00a8
xifei.wu <xifei.wu@acadine.com>
2016-09-06 10:36:07 +0800

    demo for showing html load sequence.

---
+++ static/js/load_sequence.js
@@ -0,0 +1,6 @@
+console.log('in load_sequence.js');
+// var imgs = document.querySelectorAll('img');
+// Array.prototype.slice.call(imgs).forEach(function(img) {
+//   console.log(img);
+// })
+// addListenerForImg();
